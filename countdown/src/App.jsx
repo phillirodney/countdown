f7fa0e57ceed950f018c3e82c3c4e98ad52c84aa
PHILLICHILLI <PHILLICHILLI>
2017-06-28 09:46:38 +0100

    localhost:3000 now reads and displays the div in app.jsx

---
+++ countdown/src/App.jsx
@@ -0,0 +1,12 @@
+import React , { Component } from 'react';
+
+class App extends Component {
+    
+    render() {
+        return(
+        <div>Countdown, App Component </div>
+        )
+    }
+}
+
+export default App;
