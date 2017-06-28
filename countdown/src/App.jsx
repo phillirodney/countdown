1e676dd587b664971545ea599db5281371f7511f
PHILLICHILLI <PHILLICHILLI>
2017-06-28 10:34:07 +0100

    onclick function added and change deadline method created, counter date changes when button is clicked

--- countdown/src/App.jsx
+++ countdown/src/App.jsx
@@ -3,12 +3,27 @@ import './App.css';
 //ES6 class
 class App extends Component {
 
+    constructor(props) {
+        super(props); 
+        this.state = {
+                deadline: 'July 29, 2017'
+        }
+    }
+    
+    changeDeadline() {
+        //never mutate or change state DIRECTLY
+        //this.state.deadline = 'November 25, 2017' is BAD
+        this.setState({deadline: 'November 25, 2017'})
+        //instead we use key value pairs, stay bug free	
+    }
+    
     render() {
         return (
             //adding structure for the page
             <div className="App">
-                <div className="App-title">Countdown to Romance in Rome, 2017!</div>
-            <div>
+                <div className="App-title">Countdown to {this.state.deadline}</div>
+            <div className="App-title">Romance in Rome!</div>
+                <div>
                     <div className="Clock-days">14 Days</div>
                     <div className="Clock-hours">30 Hours</div>
                     <div className="Clock-minutes">15 Minutes</div>
@@ -16,8 +31,8 @@ class App extends Component {
                 </div>
                 <div>
                     <input placeholder='new date' />
-                    <button>Submit</button>
-                </div>
+                    <button onClick={() => this.changeDeadline()}>Submit</button>
+                    </div>
 
             </div>
         )
@@ -25,4 +40,6 @@ class App extends Component {
 }
 
 export default App;
+//In onclick we use an ES6 anon function, handlers will loop and cause code to crash otherwise. 
+
 //Thanks to create-react-app when we save the project it auto refreshes in localhost:8080
